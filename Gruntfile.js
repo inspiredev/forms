92d3662e555ebd3fca5ef6b63684994ab06e5a10
Tri Nguyen <tri@tridnguyen.com>
2013-11-29 21:05:43 -0500

    add sass and grunt tasks for sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,9 +6,10 @@ module.exports = function(grunt) {
 	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
 	grunt.initConfig({
+		config: require('./config/config'),
 		concurrent: {
 			dev: {
-				tasks: ['nodemon'],
+				tasks: ['nodemon', 'watch'],
 				options: {
 					logConcurrentOutput: true
 				}
@@ -16,13 +17,51 @@ module.exports = function(grunt) {
 		},
 		nodemon: {
 			dev: {
-
+			}
+		},
+		sass: {
+			dev: {
+				files: {
+					'public/css/main.css': 'sass/main.scss'
+				}
+			}
+		},
+		autoprefixer: {
+			dev: {
+				src: 'public/css/main.css',
+				dest: 'public/css/main.css'
+			}
+		},
+		csso: {
+			options: {
+				report: 'gzip'
+			},
+			prod: {
+				files: {
+					'public/css/main.css': ['public/components/normalize-css/normalize.css', 'public/css/main.css']
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: ['sass/**/*.scss'],
+				tasks: ['css']
 			}
 		}
 
 	});
 
-	grunt.registerTask('dev', ['concurrent:dev']);
+	grunt.registerTask('css', [
+		'sass',
+		'autoprefixer',
+		'csso'
+	]);
+
+	grunt.registerTask('dev', [
+		'css',
+		'concurrent:dev'
+	]);
+
 
 	grunt.registerTask('default', ['dev']);
 }
