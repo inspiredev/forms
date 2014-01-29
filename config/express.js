ed87d3db1ec63e0be1f0071a16fd3c668a5a3874
Tri Nguyen <tri@tridnguyen.com>
2014-01-28 23:18:26 -0500

    add helpers to handlebars engine

--- config/express.js
+++ config/express.js
@@ -2,17 +2,25 @@ var express = require('express'),
 	exphbs = require('express3-handlebars');
 
 module.exports = function(app, config) {
+	// hbs enging
+	var hbs = exphbs.create({
+		extname: '.hbs',
+		layoutsDir: config.root + '/app/views/layouts',
+		defaultLayout: 'main',
+		helpers: {
+			debug: function(stuff) {
+				console.log(stuff);
+			}
+		}
+	})
+
 	app.configure(function () {
 		app.use(express.compress());
 		app.set('port', config.port);
 		app.use(express.logger('dev'));
 		app.use(express.bodyParser());
 		app.use(app.router);
-		app.engine('.hbs', exphbs({
-			extname: '.hbs',
-			layoutsDir: config.root + '/app/views/layouts',
-			defaultLayout: 'main'
-		}));
+		app.engine('.hbs', hbs.engine);
 		app.set('views', config.root + '/app/views');
 		app.set('view engine', '.hbs');
 		app.use(express.static(config.root + '/public'));
