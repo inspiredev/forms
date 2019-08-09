0a9dbc46d14b408461eea984c95fd3b28fdeaeff
Tri Nguyen <tri@tridnguyen.com>
2019-08-08 22:42:25 -0400

    style fix

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -2,7 +2,7 @@ const Rollbar = require('rollbar');
 
 module.exports = new Rollbar({
 	accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
-	captureUncaught:true,
-	captureUnhandledRejections:true,
+	captureUncaught: true,
+	captureUnhandledRejections: true,
 	environment: process.env.NODE_ENV
 });
