a751fe88a1833231771349682596bd9cee33e89b
Tri Nguyen <tri@tridnguyen.com>
2019-08-08 22:23:57 -0400

    Add rollbar environment

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -3,5 +3,6 @@ const Rollbar = require('rollbar');
 module.exports = new Rollbar({
 	accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
 	captureUncaught:true,
-	captureUnhandledRejections:true
+	captureUnhandledRejections:true,
+	environment: process.env.NODE_ENV
 });
